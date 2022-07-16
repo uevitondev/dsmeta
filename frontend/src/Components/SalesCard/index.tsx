60b8163ba0ce00fd8753bd713bcf2e0c9a3e3cf6
Ueviton Santos <uevitonsantos@outlook.com>
2022-07-16 15:16:02 -0300

    Axios, useEffect first request

--- frontend/src/Components/SalesCard/index.tsx
+++ frontend/src/Components/SalesCard/index.tsx
@@ -2,7 +2,8 @@ import NotificationButton from '../NotificationButton';
 import './styles.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SalesCard() {
 
@@ -11,6 +12,13 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
+
+    useEffect (() => {
+        axios.get("http://localhost:8080/sales?minDate=2021-11-01&maxDate=2021-12-31")
+        .then(response => {
+            console.log(response.data)
+        });
+    }, [])
 
     return (
         <>
